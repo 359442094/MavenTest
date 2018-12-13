eacec2bf9cf7716775d57dcb5341d7a52044b2f4
359442094 <359442094@qq.com>
2018-12-13 09:01:39 +0800

    新增调用接口工具类

--- src/main/java/sk/toInterface/interFaceUtil.java
+++ src/main/java/sk/toInterface/interFaceUtil.java
@@ -1,10 +1,10 @@
 package sk.toInterface;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+/**
+ * 接口工具类
+ * */
 public class interFaceUtil {
 
         /**
@@ -12,56 +12,45 @@ public class interFaceUtil {
          * @param path 对方或第三方提供的路径
          * @param data 向对方或第三方发送的数据，大多数情况下给对方发送JSON数据让对方解析
          */
-        public static void interfaceUtil(String path,String data) {
-            try {
-                URL url = new URL(path);
-                //打开和url之间的连接
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                PrintWriter out = null;
-                //请求方式
-                conn.setRequestMethod("GET");
-                //设置通用的请求属性
-                conn.setRequestProperty("accept", "*/*");
-                conn.setRequestProperty("connection", "Keep-Alive");
-                conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)");
-                //设置是否向httpUrlConnection输出，设置是否从httpUrlConnection读入，此外发送post请求必须设置这两个
-                //最常用的Http请求无非是get和post，get请求可以获取静态页面，也可以把参数放在URL字串后面，传递给servlet，
-                //post与get的 不同之处在于post的参数不是放在URL字串里面，而是放在http请求的正文内。
-                conn.setDoOutput(true);
-                conn.setDoInput(true);
-                //获取URLConnection对象对应的输出流
-                out = new PrintWriter(conn.getOutputStream());
-                //发送请求参数即数据
-                out.print(data);
-                //缓冲数据
-                out.flush();
-                //获取URLConnection对象对应的输入流
-                InputStream is = conn.getInputStream();
-                //构造一个字符流缓存
-                BufferedReader br = new BufferedReader(new InputStreamReader(is));
-                String str = "";
-                while ((str = br.readLine()) != null) {
-                    System.out.println(str);
-                }
-                //关闭流
-                is.close();
-                //断开连接，最好写上，disconnect是在底层tcp socket链接空闲时才切断。如果正在被其他线程使用就不切断。
-                //固定多线程的话，如果不disconnect，链接会增多，直到收发不出信息。写上disconnect后正常一些。
-                conn.disconnect();
-                System.out.println("完整结束");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        public static void main(String[] args) throws IOException {
+            String jsonStr="11111";
+            String path="http://gc.ditu.aliyun.com/geocoding?a=祁东县"; //http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo"
+            String reuslt = sendPost(jsonStr, path);
+            System.out.println(reuslt);
         }
 
         /**
-         * 备用测试地址：
-         * 　　手机信息查询接口：http://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=手机号 　　　　　　　　　　　
-         * 　　阿里云根据地区名获取经纬度接口：http://gc.ditu.aliyun.com/geocoding?a=兰州市
-         * */
-        public static void main(String[] args) {
-            interfaceUtil("http://gc.ditu.aliyun.com/geocoding?a=祁东县","");
+        * 备用测试地址：
+        * 　　手机信息查询接口：http://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=手机号 　　　　　　　　　　　
+        * 　　阿里云根据地区名获取经纬度接口：http://gc.ditu.aliyun.com/geocoding?a=兰州市
+        * */
+        public static String sendPost(String jsonStr, String path)
+                throws IOException {
+            byte[] data = jsonStr.getBytes();
+            URL url = new URL(path);
+            HttpURLConnection conn =(java.net.HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setConnectTimeout(5 * 1000);// 设置连接超时时间为5秒
+            conn.setReadTimeout(20 * 1000);// 设置读取超时时间为20秒
+            // 使用 URL 连接进行输出，则将 DoOutput标志设置为 true
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type", "text/json;charset=UTF-8");
+            //conn.setRequestProperty("Content-Encoding","gzip");
+            conn.setRequestProperty("Content-Length", String.valueOf(data.length));
+            OutputStream outStream = conn.getOutputStream();// 返回写入到此连接的输出流
+            outStream.write(data);
+            outStream.close();//关闭流
+            String msg = "";// 保存调用http服务后的响应信息
+            // 如果请求响应码是200，则表示成功
+            if (conn.getResponseCode() == 200) {
+                // HTTP服务端返回的编码是UTF-8,故必须设置为UTF-8,保持编码统一,否则会出现中文乱码
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        (InputStream) conn.getInputStream(), "UTF-8"));
+                msg = in.readLine();
+                in.close();
+            }
+            conn.disconnect();// 断开连接
+            return msg;
         }
-
 }
 
